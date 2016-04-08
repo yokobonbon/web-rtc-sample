1fd887c4275ea2fbb42e4f37b79c48033c27e133
Daisuke Imiya <daisuke.imiya@sonymobile.com>
2016-04-08 19:49:30 +0900

    Start to use listAllPeers

--- videochat/script.js
+++ videochat/script.js
@@ -16,7 +16,6 @@ function MediaController(view_controller) {
   var localStream = null;
 
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
   navigator.getUserMedia({video: true, audio: true},
     function(stream) {
       view_controller.setMyVideo(stream);
@@ -33,7 +32,7 @@ function MediaController(view_controller) {
 }
 
 function PeerController(view_controller, media_controller) {
-  var peer = new Peer({key: '<your api key>', debug: 3});
+  var peer = new Peer({key: '<your API key>', debug: 3});
   var dataConnection = null;
 
   peer.on('open', function(id){
@@ -42,24 +41,36 @@ function PeerController(view_controller, media_controller) {
   });
 
   peer.on('call', function(media){
-      media.answer(media_controller.getLocalStream());
-      media.on('stream', function(stream) {
-        view_controller.setYourVideo(stream);
-      });
+    media.answer(media_controller.getLocalStream());
+    media.on('stream', function(stream) {
+      view_controller.setYourVideo(stream);
+    });
+  });
+
+  peer.on('close', function(){
+    peer.destory();
+  });
+
+  peer.on('error', function(err){
+    alert(err);
   });
 
   function call(peerid) {
     var media = peer.call(peerid, media_controller.getLocalStream());
     media.on('stream', function(stream) {
-        view_controller.setYourVideo(stream);
+      view_controller.setYourVideo(stream);
     });
   }
 
   return {
     startCall : function() {
-      var callto = document.getElementById('callto');
-      call(callto.value);
-      dataConnection = peer.connect(callto.value);
+      peer.listAllPeers(function(remoteIds) {
+        if (remoteIds.length > 0) {
+          call(remoteIds[0]);
+        } else {
+          alert("remote not found");
+        }
+      });
     }
   }
 }
